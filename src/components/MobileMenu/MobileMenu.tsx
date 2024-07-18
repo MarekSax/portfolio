2d94f0e4f60bc9c828eef0329b706faf8a28060a
MarekSax <mareksax@wp.pl>
2024-07-18 16:34:07 +0200

    feat: add open and close functionality for mobile menu

--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -1,3 +1,37 @@
+import { useState } from 'react';
+import styles from './MobileMenu.module.scss';
+import { IoMenu, IoClose } from 'react-icons/io5';
+import cn from 'classnames';
+
 export const MobileMenu = () => {
-  return <div>MobileMenu</div>;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const openMenu = () => {
+    setIsMenuOpen(true);
+    document.body.style.overflow = 'hidden';
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+    document.body.style.overflow = 'auto';
+  };
+
+  return (
+    <>
+      <div className={styles.mobileMenu}>
+        <IoMenu size={48} onClick={openMenu} />
+      </div>
+      <div
+        className={cn(styles.mobileMenuContent, {
+          [styles.mobileMenuOpen]: isMenuOpen,
+        })}
+      >
+        <IoClose
+          size={48}
+          onClick={closeMenu}
+          className={styles.mobileMenuIcon}
+        />
+      </div>
+    </>
+  );
 };
