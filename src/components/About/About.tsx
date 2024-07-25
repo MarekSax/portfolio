dc363e05c4bbe7808b4bf2c336c2c3f4c40d8040
MarekSax <mareksax@wp.pl>
2024-07-25 17:44:50 +0200

    feat: add 'about' section content

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,8 +1,17 @@
 import styles from './About.module.scss';
+import { marek } from '../../editable_stuff/pageContent';
 export const About = () => {
   return (
     <section id="about" className={styles.about}>
-      About
+      <article className={styles.content}>
+        <div className={styles.content__avatar}>
+          <img src="./images/avatar.jpg" alt="" />
+        </div>
+        <div className={styles.content__description}>
+          <h1>Hi, I'm Marek</h1>
+          {marek.about}
+        </div>
+      </article>
     </section>
   );
 };
